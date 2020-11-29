777f96c40ea42a83a7a6fa05552bd633498ef2d5
VitalieVSV <dut66AHJCT>
2020-11-29 17:54:58 +0200

    Frame

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,4 +1,23 @@
 package GUI;
 
+import javax.swing.*;
+
 public class Main {
+    private JButton button1;
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        Main form = new Main();
+        form.initComponents();
+    }
+
+    private void initComponents(){
+        JFrame frame = new JFrame("Socket");
+        frame.setContentPane(new Main().mainPanel);
+        frame.setAlwaysOnTop(true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
